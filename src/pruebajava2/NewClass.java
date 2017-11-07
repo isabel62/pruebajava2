e29b5b7d466cf7fa42a985c8dc013b6f3105d760
isabel62 <isabeliribarreng@hotmail.com>
2017-11-06 22:06:23 -0300

    ingresar y ver los ampos que se debe ingresar

---
+++ src/pruebajava2/NewClass.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LAB08
+ */
+public class NewClass {¨
+        
+  public static void main (String`[]args);
+          {
+  
+ int forJOptionPane.showMessageDialog("INGRESE CONTRACEÑA");
+ 
+int forJOptionPane.showMessageDialog("INGRESE NUEVAMENTE");
+
+int forJOptionPane.showMessageDialog("CONTRACEÑA INCORRESTA");
+
+int forJOptionPane.showMessageDialog("INGRESE NUEVAMENTE"); 
+
+int boucle,OptionPane.showMessageDialog();
+
+OptionPane.showMessageDialog(); 
+       
+
+
+
+
+default;
+}
+}
